99b8eaf73b182b7ceab10b889b1e3e9b9a85a630
Ryan Barker <ryan.dean.barker@gmail.com>
2022-04-09 14:50:03 -0400

    starter landmark data

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -43,5 +43,14 @@ CREATE TABLE landmark(
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO address (address_line_1, city, state, zipcode) VALUES 
+('St Charles Avenue', 'New Orleans', 'LA', 70130),
+('615 Pere Antonie Aly', 'New Orleans', 'LA', 70116),
+('7 Bamboo Rd', 'New Orleans', 'LA', 70124);
+
+INSERT INTO landmark(name, address_id, description) VALUES
+('Garden District', 1, 'An area of the city that features numerous historic homes.'),
+('St. Louis Cathedral', 2, 'The major landmark of the French Quarter is the oldest continuously active cathedral in the U.S.'),
+('Longue Vue House and Gardens', 3, 'Longue Vue House and Gardens is a multifaceted historic estate featuring a world-class house museum and eight acres of stunning gardens that include an interactive Discovery Garden for children of all ages.');
 
 COMMIT TRANSACTION;
