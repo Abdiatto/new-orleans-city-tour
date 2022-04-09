8d4a1740c3556abe99cb392cea804d840acdc11c
Ryan Barker <ryan.dean.barker@gmail.com>
2022-04-09 14:29:07 -0400

    landmark and address table

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS address;
+DROP TABLE IF EXISTS landmark;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -17,6 +19,26 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE address (
+    address_id serial primary key,
+    address_line_1 varchar(255) not null,
+    address_line_2 varchar(255),
+	city varchar(50) not null,
+	state varchar(2) not null,
+	zipcode int not null
+);
+
+
+CREATE TABLE landmark(
+	landmark_id serial primary key,
+	name varchar(260) not null,
+	description text,
+    address_id  bigint not null,
+
+    constraint fk_address_id foreign key (address_id) references address(address_id)  
+);
+
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
