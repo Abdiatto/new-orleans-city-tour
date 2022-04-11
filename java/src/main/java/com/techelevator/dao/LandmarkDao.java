f25e93bc0b1e9650c4936c21422c7e890a089694
Berhane [D[D[D[D[D[D[D[DTekle  Berhane <Tekleberhane95@gmail.com>
2022-04-11 10:49:12 -0400

    restart

--- java/src/main/java/com/techelevator/dao/LandmarkDao.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.dao;
-
-import com.techelevator.controller.LandmarkController;
-import com.techelevator.model.LandMark;
-
-public interface LandMarkDao {
-    LandMark get(int landMarkId);
-    LandMark add(LandMark landMark);
-}
