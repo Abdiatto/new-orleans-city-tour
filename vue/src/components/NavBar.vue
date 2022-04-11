91882d4814037b9c3e92333230e5cfec6eeb70e5
Ryan Barker <ryan.dean.barker@gmail.com>
2022-04-11 09:18:23 -0400

    broke out nav into seperate component

---
+++ vue/src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link v-bind:to="{ name: 'home' }">Logo Placeholder</router-link>
+      </li>
+      <li><router-link v-bind:to="{ name: 'add' }">Add</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "nav-bar",
+  data() {
+    return {
+      showMenu: true,
+    };
+  },
+  computed: {},
+};
+</script>
+
+<style>
+nav {
+  padding: 0.5rem;
+}
+</style>
