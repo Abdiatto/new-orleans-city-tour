97822145209631844c70a63c9c85dbad416cb84c
Will Rees <willrees353@gmail.com>
2022-04-14 14:13:55 -0400

    moreObjectsMoreProblems

--- java/src/main/java/com/techelevator/dao/ItineraryDao.java
+++ java/src/main/java/com/techelevator/dao/ItineraryDao.java
@@ -1,9 +1,14 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Itinerary;
+import com.techelevator.model.ItineraryAddDTO;
+
 public interface ItineraryDao {
 
-    public void deleteItinerary (int itineraryID);
-    public void deleteItineraryLandmark (int itineraryID, int landmarkID);
+    void deleteItinerary (int itineraryID);
+    void deleteItineraryLandmark (int itineraryID, int landmarkID);
+    Itinerary add(Itinerary itinerary);
+    void update(ItineraryAddDTO itineraryAddDTO, Integer itineraryID);
 
 
 }
